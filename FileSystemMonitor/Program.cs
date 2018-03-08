18064571a1f2e7fffd0ff19bb6aab4b40450f979
楊明勳 Ming <ming@miniasp.com>
2018-03-08 17:58:57 +0800

    加入專案檔案。

---
+++ FileSystemMonitor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
