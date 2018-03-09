b90df6917a4fbb99709894cf4dbc11c0cfcabadb
楊明勳 Ming <ming@miniasp.com>
2018-03-09 17:42:28 +0800

    版本建立

---
+++ FileSystemMonitor/CmdHelper.cs
@@ -0,0 +1,85 @@
+﻿using System.Diagnostics;
+
+namespace FileSystemMonitor
+{
+    /// <summary>
+    ///     命令提示字元Helper
+    /// </summary>
+    public class CmdHelper
+    {
+        /// <summary>
+        ///     建構子
+        /// </summary>
+        /// <param name="isShowWindow">是否顯示命令提示視窗</param>
+        public CmdHelper(bool isShowWindow = false)
+        {
+            IsShowWindow = isShowWindow;
+        }
+
+        /// <summary>
+        ///     是否顯示命令提示視窗
+        /// </summary>
+        private bool IsShowWindow { get; }
+
+        /// <summary>
+        ///     執行命令提示字元並回傳執行結果文字
+        /// </summary>
+        /// <param name="cmdText">指令</param>
+        /// <returns>結果文字</returns>
+        public string ExecCmd(string cmdText)
+        {
+            var cmd = new Process();
+
+            var startInfo = GetProStartInfo(cmdText, true);
+
+            cmd.StartInfo = startInfo;
+            cmd.Start();
+
+            var strOutput = cmd.StandardOutput.ReadToEnd();
+            cmd.WaitForExit();
+
+            return strOutput;
+        }
+
+        /// <summary>
+        ///     執行命令提示字元
+        /// </summary>
+        /// <param name="cmdText">指令</param>
+        public void ExecNonCmd(string cmdText)
+        {
+            var cmd = new Process();
+
+            var startInfo = GetProStartInfo(cmdText, false);
+
+            cmd.StartInfo = startInfo;
+            cmd.Start();
+
+            cmd.WaitForExit();
+        }
+
+        /// <summary>
+        ///     取得Process 初始參數
+        /// </summary>
+        /// <param name="cmdText">命令字元</param>
+        /// <param name="isReturnOutput">是否要輸出結果</param>
+        /// <returns></returns>
+        private ProcessStartInfo GetProStartInfo(string cmdText, bool isReturnOutput)
+        {
+            var startInfo = new ProcessStartInfo();
+
+            if (!IsShowWindow)
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+
+            if (isReturnOutput)
+                startInfo.RedirectStandardOutput = true;
+
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/c" + cmdText;
+
+            return startInfo;
+        }
+    }
+}
--- FileSystemMonitor/Program.cs
+++ FileSystemMonitor/Program.cs
@@ -1,15 +1,125 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
 
 namespace FileSystemMonitor
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static string _watchFilePath;
+        private static bool _isIncludeSubdirectories;
+        private static List<FileLog> _fileLogs;
+
+        private static void Main(string[] args)
         {
+            _watchFilePath = @"W:\Demo";
+            _isIncludeSubdirectories = true;
+            _fileLogs = new List<FileLog>();
+
+            var watcher = new FileSystemWatcher
+            {
+                Filter = "*.*",
+                Path = _watchFilePath + "\\",
+                
+                IncludeSubdirectories = _isIncludeSubdirectories,
+                NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                                                        | NotifyFilters.FileName | NotifyFilters.DirectoryName
+            };
+
+            watcher.Changed += OnChanged;
+            watcher.Created += OnChanged;
+            watcher.Deleted += OnChanged;
+            watcher.Renamed += OnRenamed;
+            watcher.EnableRaisingEvents = true;
+
+            var tmr = new Timer(1000 * 20);
+            tmr.Elapsed += delegate { SendNotify(); };
+            tmr.Start();
+
+            var command = "";
+            do
+            {
+                command = Console.ReadLine();
+            } while (command != "exit");
         }
+
+        public static void SendNotify()
+        {
+            if (!_fileLogs.Any()) return;
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //Console.WriteLine("=============File Change Notify=============");
+
+            //foreach (var model in _fileLogs)
+            //{
+            //    Console.WriteLine("=======================================");
+            //    Console.WriteLine("Type => " + model.ChangeType);
+            //    Console.WriteLine("FullPath => " + model.FullPatch);
+            //    Console.WriteLine("Time => " + model.ChangeTime.ToString("yyyy/MM/dd HH:mm"));
+            //    Console.WriteLine("=======================================");
+            //}
+
+            _fileLogs.Clear();
+
+            var cmdHelper = new CmdHelper(true);
+            cmdHelper.ExecNonCmd("W:&cd W:\\Demo&git reset --hard");
+            Console.Clear();
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.Name.StartsWith(".git\\")) return;
+            if (_fileLogs.Any(x => x.FullPatch == e.FullPath)) return;
+
+            var model = new FileLog
+            {
+                FullPatch = e.FullPath,
+                ChangeType = e.ChangeType.ToString(),
+                ChangeTime = DateTime.Now
+            };
+            _fileLogs.Add(model);
+
+            Console.WriteLine("=======================================");
+            Console.WriteLine("File Changed!!");
+            Console.WriteLine("Type => " + model.ChangeType);
+            Console.WriteLine("FullPath => " + model.FullPatch);
+            Console.WriteLine("Time => " + model.ChangeTime.ToString("yyyy/MM/dd HH:mm"));
+            Console.WriteLine("=======================================");
+        }
+
+        private static void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            if (e.Name.StartsWith(".git\\")) return;
+            if (_fileLogs.Any(x => x.FullPatch == e.FullPath)) return;
+
+            var model = new FileLog
+            {
+                FullPatch = e.OldFullPath,
+                ChangeType = e.ChangeType.ToString(),
+                ChangeTime = DateTime.Now
+            };
+            _fileLogs.Add(model);
+
+            Console.WriteLine("=======================================");
+            Console.WriteLine("File Changed!!");
+            Console.WriteLine("Type => " + model.ChangeType);
+            Console.WriteLine("FullPath => " + model.FullPatch);
+            Console.WriteLine("Time => " + model.ChangeTime.ToString("yyyy/MM/dd HH:mm"));
+            Console.WriteLine("=======================================");
+        }
+    }
+    
+
+    internal class FileLog
+    {
+        public string FullPatch { get; set; }
+
+        public string ChangeType { get; set; }
+
+        public DateTime ChangeTime { get; set; }
     }
 }
